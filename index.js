293c915500e338bc022559646e1fee5fec273ee1
JosephObote <joeobote01@gmail.com>
2023-08-09 18:48:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+const employee = {
+    name: "" ,
+    street: ""
+}
+function beforeEach(){
+    for (const key in employee) {
+        delete employee[key];
+      }
+
+      employee.name = 'Sam';
+
+}
+ function updateEmployeeWithKeyAndValue(object, key, value){
+    // const object = {... object}
+    
+
+}
